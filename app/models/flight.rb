c719dcc4f068a7b7fc5cc18f976fffd70f47ac84
mihailkulik <mkulik2407@gmail.com>
2023-10-14 13:13:36 +0300

    feat: fix  validation

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,17 +6,23 @@ class Flight < ApplicationRecord
     return if flight_number.blank?
     valid_format = /\A([A-Z0-9]{2}\d{4}|[A-Z0-9]{3}\d{4})\z/
 
-    unless flight_number.match(valid_format)
-      errors.add(:flight_number, 'is not in a valid format')
-      return
-    end
     unless flight_number.nil?
-      zzzz = flight_number[-4..-1]
+      zzzz = flight_number.gsub(/\D/, '')
+      puts zzzz
       if zzzz.length < 4
-        padded_zzzz = zzzz.rjust(4, '0')
-        self.flight_number = flight_number[0..-5] + padded_zzzz
+        padded_zzzz = zzzz.ljust(4, '0')
+        puts padded_zzzz
+        puts flight_number.gsub(/[^A-Za-z]/, '').upcase
+        puts flight_number.gsub(/[^A-Za-z]/, '').upcase + padded_zzzz
+        self.flight_number = flight_number.gsub(/[^A-Za-z]/, '').upcase + padded_zzzz
       end
     end
+    unless flight_number.match(valid_format)
+      errors.add(:flight_number, 'is not in a valid format')
+      nil
+    end
   end
+
+
 
 end
